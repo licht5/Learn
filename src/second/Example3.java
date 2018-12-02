43eaa73f60e1a936edcacd5de9a3afe1c6769a68
licht5 <1961807207@qq.com>
2018-12-02 12:36:41 +0800

    test add

--- src/second/Example3.java
+++ src/second/Example3.java
@@ -4,6 +4,9 @@ public class Example3 {
     public static void main(String[] args){
         Single s1=Single.getInstance();
         Single s2=Single.getInstance();
+
+
+        System.out.println("add");
         System.out.println(s1==s2);
 
     }
